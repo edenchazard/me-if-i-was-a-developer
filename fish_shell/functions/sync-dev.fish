1d2020c4f38a3a0cb8f3da7de29d0ebd89a7fd83
edenchazard <neon.fortytwo@gmail.com>
2026-02-26 23:41:13 +0000

    automation for nerds

---
+++ fish_shell/functions/sync-dev.fish
@@ -0,0 +1,6 @@
+function sync-dev
+    cp /mnt/c/Users/neonf/AppData/Roaming/Code/User/settings.json ./vs_code
+    cp -r $__fish_config_dir/functions ./fish_shell
+    cp -r $__fish_config_dir/config.fish ./fish_shell
+    powershell.exe -WindowStyle Hidden -NonInteractive -Command "code --list-extensions" | xargs -L 1 echo code --install-extension > vs_code/extensions
+end
